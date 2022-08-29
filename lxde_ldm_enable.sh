75f1303304d8d2fa27b49782b6238d1c4796ae94
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-08-29 11:56:36 -0300

    ldm

--- lxde_ldm_enable.sh
+++ lxde_ldm_enable.sh
@@ -1,1 +1,7 @@
 # in a jetson nano this will save you around 1GB of RAM usage
+if [ "${DEFAULT_DM}" != "/usr/sbin/lightdm" ]; then
+                        echo "/usr/sbin/lightdm" > "/etc/X11/default-display-manager"
+                        wait_debconf_resource
+                        DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true dpkg-reconfigure lightdm
+                        echo set shared/default-x-display-manager lightdm | debconf-communicate
+                fi
