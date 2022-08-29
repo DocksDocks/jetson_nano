bd79e8e9f49df8f37099747453d84077c1ffff58
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-08-29 11:50:53 -0300

    nano utils

---
+++ lxde_ldm_enable.sh
@@ -0,0 +1,1 @@
+# in a jetson nano this will save you around 1GB of RAM usage
---
+++ swap_boost.sh
@@ -0,0 +1,5 @@
+sudo fallocate -l "$1"G /mnt/"$1"GB.swap \
+&& sudo mkswap /mnt/"$1"GB.swap \
+&& sudo swapon /mnt/"$1"GB.swap \
+&& /mnt/"$1"GB.swap  none  swap  sw 0  0 \
+&& sudo swapon -s
