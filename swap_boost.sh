555ecccb9ae3e9472739e8ab1e82b18e151b2995
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-08-31 14:56:06 -0300

    Update swap_boost.sh

--- swap_boost.sh
+++ swap_boost.sh
@@ -1,5 +1,9 @@
-sudo fallocate -l "$1"G /mnt/"$1"GB.swap \
+sudo systemctl disable nvzramconfig \
+&& sudo fallocate -l "$1"G /mnt/"$1"GB.swap \
+&& sudo chmod 600 /mnt/"$1"GB.swap \
+&& sudo chown 0.0 /mnt/"$1"GB.swap \
+&& sudo swapoff -a \
 && sudo mkswap /mnt/"$1"GB.swap \
 && sudo swapon /mnt/"$1"GB.swap \
-&& sudo /mnt/"$1"GB.swap  none  swap  sw 0  0 \
+&& sudo echo "/mnt/"$1"GB.swap none swap defaults 0  0" >> /etc/fstab \
 && sudo swapon -s
