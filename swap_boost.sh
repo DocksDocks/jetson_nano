ace1eceed523b2dfd66d3e7f08488cf302bd6ecf
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-08-31 15:04:54 -0300

    Update swap_boost.sh

--- swap_boost.sh
+++ swap_boost.sh
@@ -1,14 +1,15 @@
 sudo systemctl disable nvzramconfig
-if [ /mnt/"$1"GB.swap] then
+if [ /mnt/"$1"GB.swap ] then
  sudo swapoff -a \
-&& sudo mkswap /mnt/"$1"GB.swap \
-&& sudo swapon /mnt/"$1"GB.swap
+ && sudo mkswap /mnt/"$1"GB.swap \
+ && sudo swapon /mnt/"$1"GB.swap
+else 
+ sudo fallocate -l "$1"G /mnt/"$1"GB.swap \ 
+ && sudo chmod 600 /mnt/"$1"GB.swap \
+ && sudo chown 0.0 /mnt/"$1"GB.swap \
+ && sudo swapoff -a \
+ && sudo mkswap /mnt/"$1"GB.swap \
+ && sudo swapon /mnt/"$1"GB.swap \
+ # && sudo echo "/mnt/"$1"GB.swap none swap sw 0  0" >> /etc/fstab \
+ && sudo swapon -s
 fi
-sudo fallocate -l "$1"G /mnt/"$1"GB.swap \
-&& sudo chmod 600 /mnt/"$1"GB.swap \
-&& sudo chown 0.0 /mnt/"$1"GB.swap \
-&& sudo swapoff -a \
-&& sudo mkswap /mnt/"$1"GB.swap \
-&& sudo swapon /mnt/"$1"GB.swap \
-# && sudo echo "/mnt/"$1"GB.swap none swap sw 0  0" >> /etc/fstab \
-&& sudo swapon -s
