1d541be0bc72b634493bac44ed5426309ba6e08c
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-08-31 15:11:13 -0300

    Update swap_boost.sh

--- swap_boost.sh
+++ swap_boost.sh
@@ -4,7 +4,7 @@ if [ -d "/mnt/""$1""GB.swap" ]; then
  && sudo mkswap /mnt/"$1"GB.swap \
  && sudo swapon /mnt/"$1"GB.swap
 else 
- sudo fallocate -l "$1"G /mnt/"$1"GB.swap \ 
+ fallocate -l "$1"G /mnt/"$1"GB.swap \ 
  && sudo chmod 600 /mnt/"$1"GB.swap \
  && sudo chown 0.0 /mnt/"$1"GB.swap \
  && sudo swapoff -a \
